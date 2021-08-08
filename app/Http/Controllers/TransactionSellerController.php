b356a81aebd6c867408bdf45da5cfacd6e34e750
Ram Poptani <poptaniram20@gmail.com>
2021-08-08 21:33:23 +0530

    chore : Added index method in TransactionSellerController

--- app/Http/Controllers/TransactionSellerController.php
+++ app/Http/Controllers/TransactionSellerController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 
 class TransactionSellerController extends Controller
 {
-    //
+    public function index(Transaction $transaction)
+    {
+        $seller = $transaction->product->seller;
+        return $this->showOne($seller);
+    }
 }
