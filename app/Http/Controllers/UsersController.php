56f32e2c12e1a9e9cc57ee3fe95da9c160d812a7
Ram Poptani <poptaniram20@gmail.com>
2021-08-08 17:37:15 +0530

    done : Users REST working

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,9 +2,88 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    //
+
+    public function index()
+    {
+        $users = User::all();
+        return response()->json(['data' => $users, 'code' => 200], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $rules = [
+            'name' => 'required|min:5',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8|confirmed'
+        ];
+        $this->validate($request, $rules);
+
+        $data = $request->all();
+        $data['password'] = bcrypt($request->password);
+        $data['verified'] = User::UNVERIFIED_USER;
+        $data['verification_token'] = User::generateVerificationTokenCode();
+        $data['admin'] = User::REGULAR_USER;
+
+        $user = User::create($data);
+        return response()->json(['data' => $user, 'code' => 201], 201);
+    }
+
+    public function show(User $user)
+    {
+        return response()->json(['data' => $user, 'code' => 200], 200);
+    }
+
+    public function update(Request $request, User $user)
+    {
+        $rules = [
+            'name' => 'min:5',
+            'email' => 'email|unique:users',
+            'password' => 'min:8|confirmed',
+            'admin' => 'in' . User::REGULAR_USER .', '. User::ADMIN_USER
+        ];
+        $this->validate($request, $rules);
+
+        if ($request->has('name')) {
+            $user->name = $request->name;
+        }
+
+        if ($request->has('email')) {
+            $user->verified = User::UNVERIFIED_USER;
+            $user->verification_token = User::generateVerificationTokenCode();
+            $user->email = $request->email;
+        }
+
+        if ($request->has('password')) {
+            $user->passoword = bcrypt($request->password);
+        }
+
+        if ($request->has('admin')) {
+            if (! $user->isVerified()) {
+                return response()->json(['error' => 'Only Verified users can become admin', 'code' => 409], 409);
+            }
+            $user->admin = $request->admin;
+        }
+
+        if (! $user->isDirty()){
+            return response()->json([
+                'error' => 'You need to specify a different value to update',
+                'code' => 422
+            ], 422);
+        }
+
+        $user->save();
+        return response()->json(['data' => $user, 'code' => 200], 200);
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return response()->json(['data' => $user, 'code' => 200], 200);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::resource('buyers', \App\Http\Controllers\BuyersController::class)
     ->only('index', 'show');
+
+Route::resource('users', \App\Http\Controllers\UsersController::class)
+    ->except('create', 'edit');
