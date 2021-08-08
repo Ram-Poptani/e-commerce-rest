8d760d9fcb3aacb6c7dfc1d1ff624541e48d30eb
Ram Poptani <poptaniram20@gmail.com>
2021-08-08 21:42:33 +0530

    chore : Added index method in BuyerCategoryController

--- app/Http/Controllers/BuyerCategoryController.php
+++ app/Http/Controllers/BuyerCategoryController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Buyer;
 use Illuminate\Http\Request;
 
 class BuyerCategoryController extends Controller
 {
-    //
+
+    public function index(Buyer $buyer)
+    {
+        $categories = $buyer->transactions()
+            ->with('product.categories')
+            ->get()
+            ->pluck('product.categories')
+            ->collapse()
+            ->unique('id')
+            ->values();
+
+        return $this->showAll($categories);
+    }
+
 }
