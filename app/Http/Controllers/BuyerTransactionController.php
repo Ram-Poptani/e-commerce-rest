cb6484bcdcd84739f27bd7500c915d7ed2132b41
Ram Poptani <poptaniram20@gmail.com>
2021-08-08 21:35:11 +0530

    chore : Added index method in BuyerTransactionController

--- app/Http/Controllers/BuyerTransactionController.php
+++ app/Http/Controllers/BuyerTransactionController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 
 class BuyerTransactionController extends Controller
 {
-    //
+    public function index(Buyer $buyer)
+    {
+        $transactions = $buyer->transactions;
+        return $this->showAll($transactions);
+    }
 }
