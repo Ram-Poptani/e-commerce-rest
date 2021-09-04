ae1022979e3f14705828aaf9a0316506bfbfd406
Ram Poptani <poptaniram20@gmail.com>
2021-09-04 23:13:23 +0530

    wip : Created a Store method for Adding a Product through Seller

--- app/Http/Controllers/SellerProductController.php
+++ app/Http/Controllers/SellerProductController.php
@@ -4,7 +4,10 @@
 namespace App\Http\Controllers;
 
 
+use App\Models\Product;
 use App\Models\Seller;
+use App\Models\User;
+use Illuminate\Http\Request;
 
 class SellerProductController extends ApiController
 {
@@ -14,4 +17,25 @@ class SellerProductController extends ApiController
         $products = $seller->products;
         return $this->showAll($products);
     }
+
+    public function store(Request $request, User $seller)
+    {
+        $rules = [
+            'name' => 'required',
+            'description' => 'required',
+            'quantity' => 'required|integer|min:1',
+            'image' => 'required|image'
+        ];
+
+        $this->validate($request, $rules);
+        $data = $request->all();
+
+        $data['status'] = Product::UNAVAILABLE_PRODUCT;
+        $data['image'] = '1.jpg'; // TODO : Image Upload is yet left
+        $data['seller_id'] = $seller->id;
+
+        $product = Product::create($data);
+        return $this->showOne($product);
+    }
+    
 }
