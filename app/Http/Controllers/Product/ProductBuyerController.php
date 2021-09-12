32b84b959bf82324054e25e7548eb12aa6bc508b
Ram Poptani <poptaniram20@gmail.com>
2021-09-12 17:55:00 +0530

    wip : added index method in ProductBuyerController

---
+++ app/Http/Controllers/Product/ProductBuyerController.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Http\Controllers\Product;
+
+
+use App\Http\Controllers\ApiController;
+use App\Models\Product;
+
+class ProductBuyerController extends ApiController
+{
+
+    public function index(Product $product)
+    {
+        $buyers = $product->transactions()
+                    ->with('buyer')
+                    ->get()
+                    ->pluck('buyer')
+                    ->unique('id');
+        return $this->showAll($buyers);
+    }
+
+}
