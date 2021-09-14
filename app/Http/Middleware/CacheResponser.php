926d78e262c3f278bcf129dcf94589cddd0be439
Ram Poptani <poptaniram20@gmail.com>
2021-09-14 22:00:21 +0530

    wip : Created a new Middleware CacheResponser for Responsing the Cached data

---
+++ app/Http/Middleware/CacheResponser.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+
+class CacheResponser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $url = request()->url();
+        $queryParameters = request()->query();
+
+        ksort($queryParameters);
+
+        $queryString = http_build_query($queryParameters);
+
+        $fullUrl = "{$url}?{$queryString}";
+
+        if (Cache::has($fullUrl)) {
+            return Cache::get($fullUrl);
+        }
+
+        return $next($request);
+    }
+}
