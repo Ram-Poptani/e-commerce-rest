64d08103d867ab0d4f594930b0be05925afde099
Ram Poptani <poptaniram20@gmail.com>
2021-09-15 23:06:16 +0530

    fix : returned new Response if data is already in the cache

--- app/Http/Middleware/CacheResponser.php
+++ app/Http/Middleware/CacheResponser.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Http\Response;
 
 class CacheResponser
 {
@@ -29,7 +30,7 @@ class CacheResponser
         $fullUrl = "{$method}:{$url}?{$queryString}";
 
         if (Cache::has($fullUrl)) {
-            return Cache::get($fullUrl);
+            return new Response(Cache::get($fullUrl));
         }
 
         return $next($request);
