c96013c8f4cfda79ff766ad6bedbfa27d75508f1
Ram Poptani <poptaniram20@gmail.com>
2021-09-15 22:10:37 +0530

    wip : Registered TransformInput middleware in Kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use App\Http\Middleware\ApplicationSignatureMiddleware;
+use App\Http\Middleware\TransformInput;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -64,6 +65,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'signature' => ApplicationSignatureMiddleware::class
+        'signature' => ApplicationSignatureMiddleware::class,
+        'transform.input' => TransformInput::class
     ];
 }
