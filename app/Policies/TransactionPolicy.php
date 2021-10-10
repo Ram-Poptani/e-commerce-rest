fe4ee0f6e80892c2c37f0c2667fc01ef8ec4f78c
Ram Poptani <poptaniram20@gmail.com>
2021-10-10 18:12:52 +0530

    refactor : adding before method in all Policies

--- app/Policies/TransactionPolicy.php
+++ app/Policies/TransactionPolicy.php
@@ -10,6 +10,13 @@ class TransactionPolicy
 {
     use HandlesAuthorization;
 
+    public function before(User $user, $ability)
+    {
+        if ($user->isAdmin()) {
+            return true;
+        }
+    }
+
     public function view(User $user, Transaction $transaction)
     {
         return $this->isBuyer($user, $transaction) || $this->isSeller($user, $transaction);
